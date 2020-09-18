e7e5f2a579af8f0672507fb9e5861d6ccceb73d5
Chris <34548930+ringating@users.noreply.github.com>
2020-09-18 08:34:55 -0700

    added mouse camera controls

---
+++ Assets/scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public Transform anchorPointTransform;
+    public float followDistance = 5f;
+    public float sensitivity = 1f;
+
+    private float pitchDegrees = 0;
+    private float yawDegrees = 0;
+
+    private Vector3 cameraWorldPosition = new Vector3();
+
+    void LateUpdate()
+    {
+        pitchDegrees = Mathf.Max(-89f, Mathf.Min(89f, pitchDegrees + sensitivity * -Input.GetAxis("Mouse Y")));
+        yawDegrees = (yawDegrees + sensitivity * Input.GetAxis("Mouse X")) % 360;
+
+        cameraWorldPosition.Set(anchorPointTransform.position.x, anchorPointTransform.position.y, anchorPointTransform.position.z - followDistance);
+        cameraTransform.SetPositionAndRotation(cameraWorldPosition, Quaternion.identity);
+
+        cameraTransform.RotateAround(anchorPointTransform.position, Vector3.right, pitchDegrees);
+        cameraTransform.RotateAround(anchorPointTransform.position, Vector3.up, yawDegrees);
+    }
+}
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -6,19 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1;
     public Rigidbody rb;
+    public CameraControl cameraScript;
 
     private Vector3 vel = new Vector3();
     private float OutOfBoundsHeight = -5f;
     private Vector3 resetPosition = new Vector3(0,8,0);
 
-    // Update is called once per frame
+    private Vector3 currForward = new Vector3();
+    private Vector3 currRight = new Vector3();
+
     void FixedUpdate()
     {
-        vel.Set(
-            Input.GetAxisRaw("Horizontal") * speed,
-            rb.velocity.y,
-            Input.GetAxisRaw("Vertical") * speed
+        currForward.Set(
+            cameraScript.anchorPointTransform.position.x - cameraScript.cameraTransform.position.x,
+            0,
+            cameraScript.anchorPointTransform.position.z - cameraScript.cameraTransform.position.z
         );
+        currRight = Vector3.Cross(currForward, Vector3.up);
+
+        vel = currForward.normalized * Input.GetAxisRaw("Vertical") * speed - currRight.normalized * Input.GetAxisRaw("Horizontal") * speed;
+        vel = Vector3.ClampMagnitude(vel, speed);
+        vel.y = rb.velocity.y;
+
         rb.velocity = vel;
 
         if (rb.position.y < OutOfBoundsHeight) 
